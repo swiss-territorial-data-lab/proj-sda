340a8b2ff95e0d7ce3b24fc7d078a665cd864cfc
Clemence Herny <clemence.herny@gmail.com>
2024-03-05 15:31:35 +0000

    Create a script to mosaic images

---
+++ scripts/mosaic.py
@@ -0,0 +1,28 @@
+from rasterio.plot import show
+from rasterio.merge import merge
+import rasterio as rio
+from pathlib import Path
+
+path = Path('/mnt/data-03/cherny/git/proj-sda/output/oth-images/')
+# Path('output').mkdir(parents=True, exist_ok=True)
+output_path = '/mnt/data-03/cherny/git/proj-sda/output/oth-images/mosaic_output.tif'
+
+raster_files = list(path.iterdir())
+raster_to_mosiac = []
+
+for p in raster_files:
+    raster = rio.open(p)
+    raster_to_mosiac.append(raster)
+
+mosaic, output = merge(raster_to_mosiac)
+
+output_meta = raster.meta.copy()
+output_meta.update(
+    {"driver": "GTiff",
+        "height": mosaic.shape[1],
+        "width": mosaic.shape[2],
+        "transform": output,
+    })
+
+with rio.open(output_path, 'w', **output_meta) as m:
+    m.write(mosaic)
