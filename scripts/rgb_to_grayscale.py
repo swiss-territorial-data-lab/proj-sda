473f4e052fcad77328d53d68a3997c0dbedb61c6
Clemence Herny <clemence.herny@gmail.com>
2024-05-08 15:35:27 +0000

    Convert RGB GeoTIFF to Gray GeoTIFF

--- scripts/rgb_to_grayscale.py
+++ scripts/rgb_to_grayscale.py
@@ -2,10 +2,15 @@ import argparse
 import os
 import sys
 import time
+import subprocess
+
+import rasterio as rio
 
 from glob import glob
 from loguru import logger
+from osgeo import gdal
 from PIL import Image
+from rasterio.plot import show
 from tqdm import tqdm
 from yaml import load, FullLoader
 
@@ -26,11 +31,41 @@ def main(WORKING_DIR, OUTPUT_DIR, IMAGE_DIR):
     written_files = []
 
     images = glob(os.path.join(IMAGE_DIR, '*.tif'))
-
+    # subprocess.call('gdal_calc.py -R ./data/images/SWISSIMAGE/zoom_16/16_33871_23265_RGB.tif --R_band=1 -G ./data/images/SWISSIMAGE/zoom_16/16_33871_23265_RGB.tif --G_band=2 -B ./data/images/SWISSIMAGE/zoom_16/16_33871_23265_RGB.tif --B_band=3 --outfile=./data/images/SWISSIMAGE/zoom_16/16_33871_23265_gray_gdal_calc.tif --calc="R*0.2989+G*0.5870+B*0.1140"')
     for image in tqdm(images, desc='Convert RGB images to grayscale images', total=len(images)):
-        img = Image.open(image).convert('L')
+
+        with rio.open(image) as src:
+            r = src.read()
+            R = src.read(1)
+            G = src.read(2)
+            B = src.read(3)
+            # https://en.wikipedia.org/wiki/Grayscale#Colorimetric_(perceptual_luminance-preserving)_conversion_to_grayscale 
+            # greyscale = 0.2125*R + 0.7154*G + 0.0721*B
+            greyscale = 0.2990*R + 0.5870*G + 0.1140*B
+            # rio.plot.show(greyscale, cmap='Greys_r')
+
+        output_meta = src.meta.copy()
+        output_meta.update(
+            {"driver": "GTiff",
+                "height": r.shape[1],
+                "width": r.shape[2],
+                "count": 1,
+                "crs": src.crs
+            })
+        
         img_name = os.path.basename(image)
-        img.save(os.path.join(OUTPUT_DIR, img_name))
+
+        with rio.open(os.path.join(OUTPUT_DIR, img_name), 'w', **output_meta) as dst:
+            dst.write(greyscale, 1)
+
+        # Alternative: works fine but no GeoTiff
+        # img = Image.open(image).convert('L')
+        # img_name = os.path.basename(image)
+        # img.save(os.path.join(OUTPUT_DIR, img_name))
+
+        # Alternative: gdal fct works fine as command in terminal not in the script
+        # subprocess.call('gdal_calc.py -R 16_33871_23265_RGB.tif --R_band=1 -G 16_33871_23265_RGB.tif --G_band=2 -B 16_33871_23265_RGB.tif --B_band=3 --outfile=16_33871_23265_gray_gdal_calc.tif --calc="R*0.2989+G*0.5870+B*0.1140"')
+
 
 if __name__ == "__main__":
 
