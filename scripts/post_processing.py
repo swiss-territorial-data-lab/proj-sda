df2119861a865201484200ec6361c98fe3a520f8
Clemence Herny <clemence.herny@gmail.com>
2024-08-19 09:04:26 +0000

    Add cadaster layer to post-process

--- scripts/post_processing.py
+++ scripts/post_processing.py
@@ -5,7 +5,6 @@ import time
 import yaml
 
 import geopandas as gpd
-import numpy as np
 import pandas as pd
 import rasterio
 
@@ -36,6 +35,7 @@ if __name__ == "__main__":
     # Load input parameters
     DETECTIONS = cfg['detections']
     TILES = cfg['aoi_tiles']
+    TICINO_CAD = cfg['ticino']['cadaster']
     DEM = cfg['dem']
     SCORE = cfg['score']
     AREA = cfg['area']
@@ -45,20 +45,23 @@ if __name__ == "__main__":
     written_files = [] 
 
     # Convert input detections to a geodataframe 
-    detections = gpd.read_file(DETECTIONS)
-    detections = detections.to_crs(2056)
-    detections = detections[detections['tag']!='FN']
-    detections['area'] = detections.geometry.area 
-    detections['det_id'] = detections.index
-    total = len(detections)
+    detections_gdf = gpd.read_file(DETECTIONS)
+    detections_gdf = detections_gdf.to_crs(2056)
+    detections_gdf = detections_gdf[detections_gdf['tag']!='FN']
+    detections_gdf['area'] = detections_gdf.geometry.area 
+    detections_gdf['det_id'] = detections_gdf.index
+    total = len(detections_gdf)
     logger.info(f"{total} input shapes")
 
     tiles_gdf = gpd.read_file(TILES)
     tiles_gdf = tiles_gdf.to_crs(2056)
 
+    ticino_cad_gdf = gpd.read_file(TICINO_CAD)
+    ticino_cad_gdf = ticino_cad_gdf.to_crs(2056)
+
     # Discard polygons detected at/below 0 m and above the threshold elevation
     r = rasterio.open(DEM)
-    detections = detections.loc[detections['geometry'].is_valid, :] 
+    detections = detections_gdf.loc[detections_gdf['geometry'].is_valid, :] 
     row, col = r.index(detections.centroid.x, detections.centroid.y)
     values = r.read(1)[row, col]
     detections['elevation'] = values 
@@ -77,14 +80,14 @@ if __name__ == "__main__":
         detections_temp_buffer_gdf = detections_temp_gdf.copy()
         detections_temp_buffer_gdf['geometry'] = detections_temp_gdf.geometry.buffer(DISTANCE, resolution=2)
 
-        detections_tiles_join = gpd.sjoin(tiles_gdf, detections_temp_buffer_gdf, how='left', predicate='contains')
-        detections_tiles_join.to_file(f'{DETECTIONS[:-5]}_test.gpkg')
+        detections_tiles_join_gdf = gpd.sjoin(tiles_gdf, detections_temp_buffer_gdf, how='left', predicate='contains')
+        # detections_tiles_join_gdf.to_file(f'{DETECTIONS[:-5]}_test.gpkg')
      
         remove_det_list = []
 
-        for tile_id in detections_tiles_join.id.unique():
+        for tile_id in detections_tiles_join_gdf.id.unique():
 
-            detection_tiles = detections_tiles_join.copy()
+            detection_tiles = detections_tiles_join_gdf.copy()
             detection_tiles_temp = detection_tiles[detection_tiles['id']==tile_id]  
             remove_det_list.extend(detection_tiles_temp.loc[:, 'det_id'].tolist()) 
 
@@ -148,13 +151,22 @@ if __name__ == "__main__":
     ta = len(detections_area)
     logger.info(f"{sc - ta} detections were removed by area filtering (area threshold = {AREA} m2)")
 
+    # # Discard polygons intersecting relevant layers
+
+    detections_area['det_id'] = detections_area.index
+    detections_area['geometry'] = detections_area.geometry.buffer(-8)
+    ticino_cad_gdf['cad_id'] = ticino_cad_gdf.index
+
+    detections_join = gpd.sjoin(detections_area, ticino_cad_gdf, how='left', predicate='intersects')
+    detections_filtered = detections_join[detections_join.cad_id.isnull()].copy()
+    detections_filtered['geometry'] = detections_filtered.geometry.buffer(8)
+
     # Final gdf
-    detection_filtered = detections_area
-    logger.info(f"{len(detection_filtered)} detections remaining after filtering")
+    logger.info(f"{len(detections_filtered)} detections remaining after filtering")
 
     # Formatting the output name of the filtered detection  
     feature = f'{DETECTIONS[:-5]}_threshold_score-{SCORE}_area-{int(AREA)}_elevation-{int(ELEVATION)}_distance-{int(DISTANCE)}'.replace('0.', '0dot') + '.gpkg'
-    detection_filtered.to_file(feature)
+    detections_filtered.to_file(feature)
 
     written_files.append(feature)
     logger.success(f"{DONE_MSG} A file was written: {feature}")  
