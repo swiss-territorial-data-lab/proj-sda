654b94013dc3096af31da1e075f74cdc85cc7e17
Clemence Herny <clemence.herny@gmail.com>
2024-09-24 15:34:31 +0000

    Create get_dem.sh

---
+++ scripts/get_dem.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Download DEM tif file
+
+mkdir -p ./data/DEM/
+wget https://github.com/lukasmartinelli/swissdem/releases/download/v1.0/switzerland_dem.tif -O ./data/DEM/switzerland_dem.tif
+gdalwarp -t_srs "EPSG:2056" ./data/DEM/switzerland_dem.tif ./data/DEM/switzerland_dem_EPSG2056.tif
+rm ./data/DEM/switzerland_dem.tif
