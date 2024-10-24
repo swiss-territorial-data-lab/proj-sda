90833eac30e39ca4d4fbc2813805fbbb79f60760
Clemence Herny <clemence.herny@gmail.com>
2024-10-24 11:49:45 +0000

    Create gt_analysis

--- scripts/batch_process.sh
+++ scripts/batch_process.sh
@@ -1,12 +1,12 @@
-#  Proj quarry detection and time machine
+ #  Proj quarry detection and time machine
 ################################################################
 #  Script used to run automatically Detections workflow for quarries detection
 #  Inputs are defined in config_det.template.yaml
 
 
-echo 'Run batch processes to make quarries detection over several years'
+echo 'Run batch process to perfrom inference over several SWISSIMAGE years'
 
-for year in 1995      # list of years to process (no comma: YEAR1 YEAR2 YEAR3 ...) 
+for year in 1971 1995 2004 2020      # list of years to process (no comma: YEAR1 YEAR2 YEAR3 ...)  
 do
     echo ' '
     echo '-----------'
