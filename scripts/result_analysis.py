3e93d1f9c7477860356895724f85aa9c836448ff
Clemence Herny <clemence.herny@gmail.com>
2024-12-16 14:41:36 +0000

    Plot TP, FP and FN in one plot

--- scripts/result_analysis.py
+++ scripts/result_analysis.py
@@ -18,9 +18,9 @@ from loguru import logger
 logger = misc.format_logger(logger)
 
 
-def plot_barchart(df, cat, data):
+def plot_barchart(df, cat, min_year, max_year, data):
 
-    plt.rcParams["figure.figsize"] = (10, 5)
+    plt.rcParams["figure.figsize"] = (12, 5)
     fig, ax = plt.subplots(1, 1)
 
     if data == 'label':
@@ -31,35 +31,38 @@ def plot_barchart(df, cat, data):
         df = df[df['det_category']==cat].copy() 
         df = df[~(df.tag.isin(["FN", "wrong class", "small polygon"]))]
         year = 'year_det'
+    elif data == 'both':
+        df['category'] = df['CATEGORY'].mask(df['CATEGORY'].isna(), df['det_category'])
+        df = df[df['category']==cat].copy() 
+        df['year'] = df['year_det'].mask(df['year_det'].isna(), df['year_label'])     
+        year = 'year'
 
     df = df[[year, 'tag']].astype({year: 'int', 'tag': 'str'})
-
     df['counts'] = 1
 
     df_temp = pd.pivot_table(data=df, index=[year], columns=['tag'], values='counts', aggfunc='count')
 
-    MIN_YEAR = 1950
-    MAX_YEAR = 2025
-
-    year_all_list = np.arange(MIN_YEAR, MAX_YEAR, 1, dtype=int)
+    year_all_list = np.arange(min_year, max_year, 1, dtype=int)
     year_filled_df = pd.DataFrame({year: year_all_list}).sort_values(by=year).reset_index(drop=True)
     df = year_filled_df.merge(df_temp, how='left', on=year).fillna(0)
 
-    if data == 'label':  
-        
+    if data == 'label':        
         df = df[[year, 'TP', 'FN']] 
         ax = df.plot(x=year, kind='bar', rot=0, log=False, stacked=True, color=['limegreen', 'red'], width=0.9)
     elif data == 'det':    
-
         if 'FP' not in df.keys(): 
             df = df[['TP']] 
         else:
             df = df[[year, 'TP', 'FP']] 
         ax = df.plot(x=year, kind='bar', rot=0, log=False, stacked=True, color=['limegreen', 'royalblue'], width=0.9)
-
-    for c in ax.containers:
-        labels = [int(a) if a > 0 else "" for a in c.datavalues]
-        ax.bar_label(c, label_type='center', color="black", labels=labels, fontsize=8)
+    elif data == 'both':
+        df = df[[year, 'TP', 'FN', 'FP']]
+        ax = df.plot(x=year, kind='bar', rot=0, log=False, stacked=True, color=['limegreen', 'red', 'royalblue'], width=0.9)
+    
+    ## Uncomment to add bar labels
+    # for c in ax.containers:
+    #     labels = [int(a) if a > 0 else "" for a in c.datavalues]
+    #     ax.bar_label(c, label_type='center', color="black", labels=labels, fontsize=7)
 
     plt.gca().set_yticks(plt.gca().get_yticks().tolist())
     ticks_to_use = df.index[::5]
@@ -68,9 +71,6 @@ def plot_barchart(df, cat, data):
     ax.set_xticks(ticks_to_use, labels, rotation=45, fontsize=10, ha='center')
     ax.xaxis.set_minor_locator(MultipleLocator(1))
     plt.xlabel('Year', fontweight='bold')
-
-    # plt.xticks(rotation=45, fontsize=8, ha='center')
-    # plt.xlabel(year.replace("_", " "), fontweight='bold')
 
     plt.title(cat, fontweight='bold')
     plt.legend(loc='upper left', frameon=False)    
@@ -119,6 +119,8 @@ if __name__ == "__main__":
     # Load input parameters
     WORKING_DIR = cfg['working_dir']
     DETECTIONS = cfg['detections']
+    MIN_YEAR = cfg['min_year'] if 'min_year' in cfg.keys() else 1945
+    MAX_YEAR = cfg['max_year'] if 'max_year' in cfg.keys() else 2025
 
     os.chdir(WORKING_DIR)
 
@@ -138,15 +140,16 @@ if __name__ == "__main__":
         logger.success(f"{DONE_MSG} A file was written: {feature}") 
 
     for cat in filter(None, detections_gdf.CATEGORY.unique()): 
-        feature = plot_barchart(detections_gdf, cat, data='label')
+        feature = plot_barchart(detections_gdf, cat, MIN_YEAR, MAX_YEAR, data='label')
         written_files.append(feature)
-        logger.success(f"{DONE_MSG} A file was written: {feature}")  
-        feature = plot_barchart(detections_gdf, cat, data='det')
+        feature = plot_barchart(detections_gdf, cat, MIN_YEAR, MAX_YEAR, data='det')
+        written_files.append(feature) 
+        feature = plot_barchart(detections_gdf, cat, MIN_YEAR, MAX_YEAR, data='both')
         written_files.append(feature)
-        logger.success(f"{DONE_MSG} A file was written: {feature}")  
+
     logger.info("The following files were written. Let's check them out!")
     for written_file in written_files:
-        logger.info(written_file)
+        logger.info(WORKING_DIR + written_file)
 
     # Stop chronometer  
     toc = time.time()
