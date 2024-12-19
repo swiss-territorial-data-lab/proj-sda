19e4a06119f7e639dacfe054b52c11bd4f7ad74b
GwenaelleSa <gwenaelle.salamin@gmail.com>
2024-12-19 09:10:14 +0000

    Remove dets under larger ones

--- scripts/merge_multi_models.py
+++ scripts/merge_multi_models.py
@@ -20,6 +20,19 @@ from functions.constants import DONE_MSG
 from loguru import logger
 logger = misc.format_logger(logger)
 
+def self_intersect(gdf):
+    _gdf = gdf.copy()
+    _gdf['geom'] = _gdf.geometry
+
+    overlap_detections_gdf = gpd.sjoin(_gdf, _gdf, how='inner')
+    overlap_detections_gdf = overlap_detections_gdf[
+        (overlap_detections_gdf.merged_id_left>=overlap_detections_gdf.merged_id_right) # We keep self-intersection, so that alone dets don't have a presence of 0.
+        # & (overlap_detections_gdf.det_category_left==overlap_detections_gdf.det_category_right)
+        & (overlap_detections_gdf.year_det_left==overlap_detections_gdf.year_det_right)
+    ].copy()
+
+    return overlap_detections_gdf
+
 
 if __name__ == "__main__":
 
@@ -40,21 +53,27 @@ if __name__ == "__main__":
     # Load input parameters
     WORKING_DIR = cfg['working_dir']
     OUTPUT_DIR = cfg['output_dir']
-    written_files = []
 
+    THRESHOLD = cfg['threshold']
+
+    written_files = []
     os.chdir(WORKING_DIR)
     os.makedirs(OUTPUT_DIR, exist_ok=True)
 
     logger.info('Read data...')
     detections_list = glob("*/**/merged_detections_at_0dot05_threshold.gpkg", recursive=True)
 
-    detections_dict = {}
+    nbr_dets_list = []
     detections_gdf = gpd.GeoDataFrame()
     for file in detections_list:
         model_name = os.path.dirname(file)
         tmp_gdf = gpd.read_file(file)
         tmp_gdf['model'] = os.path.dirname(file)    
         detections_gdf = pd.concat([detections_gdf, tmp_gdf], ignore_index=True)      # If prblm with Multipoly, use spatial index
+
+        nbr_dets_list.append(len(tmp_gdf))
+
+    logger.info(f"{DONE_MSG} {sum(nbr_dets_list)/len(nbr_dets_list):.2f} features were found in average for each model.")
     
     detections_gdf['merged_id'] = detections_gdf.index
     encoded_geoms = detections_gdf.geometry.to_wkb()
@@ -62,13 +81,7 @@ if __name__ == "__main__":
     
     logger.info('Intersect detections...')
     # Condsider a det to be the same object if more than 50% the same surface
-    detections_gdf['geom'] = detections_gdf.geometry
-    overlap_detections_gdf = gpd.sjoin(detections_gdf, detections_gdf, how='inner')
-    overlap_detections_gdf = overlap_detections_gdf[
-        (overlap_detections_gdf.merged_id_left>=overlap_detections_gdf.merged_id_right)
-        & (overlap_detections_gdf.det_category_left==overlap_detections_gdf.det_category_right)
-        & (overlap_detections_gdf.year_det_left==overlap_detections_gdf.year_det_right)
-    ].copy()
+    overlap_detections_gdf = self_intersect(detections_gdf)
     geom1 = overlap_detections_gdf.geom_left.tolist()
     geom2 = overlap_detections_gdf.geom_right.tolist()
     iou = []
@@ -77,7 +90,7 @@ if __name__ == "__main__":
     overlap_detections_gdf['IoU'] = iou
     overlap_detections_gdf = overlap_detections_gdf[overlap_detections_gdf['IoU']>=0.5]
 
-    # Mark detections representing the same object as a group 
+    logger.info('Mark detections representing the same object as a group...')
     groups = graphs.make_groups(overlap_detections_gdf, 'wkb_geom_left', 'wkb_geom_right')
     overlap_detections_gdf = overlap_detections_gdf.apply(lambda x: graphs.assign_groups(x, groups, 'wkb_geom_left'), axis=1)
     intersecting_detections_df = pd.concat([
@@ -86,16 +99,16 @@ if __name__ == "__main__":
     ])
     intersecting_detections_df.drop_duplicates(subset=['merged_id'], inplace=True)
 
-
-    # Count number of dets in each group
+    logger.info('Count number of dets in each group...')
     groups_df = intersecting_detections_df['group_id'].value_counts().reset_index()
     groups_df['percentage'] = groups_df['count']/len(detections_list)
 
     # Bring group info back to the detections
     tmp_gdf = pd.merge(detections_gdf, intersecting_detections_df[['merged_id', 'group_id']], how='left', on='merged_id')
     completed_detections_gdf = pd.merge(tmp_gdf, groups_df, how='left', left_on='group_id', right_on='group_id')
-    completed_detections_gdf.drop(columns=['wkb_geom', 'geom'], inplace=True)
-    completed_detections_gdf.loc[completed_detections_gdf['group_id'].isna(), 'percentage'] = 0
+    completed_detections_gdf.drop(columns=['wkb_geom'], inplace=True)
+    completed_detections_gdf.loc[completed_detections_gdf['group_id'].isna(), 'percentage'] = 0     # Currently, no det is removed before groupping
+    completed_detections_gdf['merged_score'] = completed_detections_gdf['score'] * completed_detections_gdf['percentage']
 
     filepath = os.path.join(OUTPUT_DIR, 'all_detections.gpkg')
     completed_detections_gdf.to_file(filepath, driver='GPKG', index=False)
@@ -104,11 +117,55 @@ if __name__ == "__main__":
     completed_detections_gdf.sort_values('score', inplace=True, ascending=False)
     groupped_detections_gdf = completed_detections_gdf.groupby('group_id', as_index=False).agg('first')
 
+    logger.info('Remove detections inside other detections...')
+    filtered_groupped_dets_gdf = groupped_detections_gdf[groupped_detections_gdf.merged_score>THRESHOLD]
+    removed_dets_gdf = groupped_detections_gdf[groupped_detections_gdf.merged_score<=THRESHOLD]
+    overlap_detections_gdf = self_intersect(filtered_groupped_dets_gdf[['geometry', 'merged_id', 'year_det', 'det_category']])
+    overlap_detections_gdf = overlap_detections_gdf[overlap_detections_gdf.merged_id_left!=overlap_detections_gdf.merged_id_right]  # Remove self-intersection
+    intersected_geoms = overlap_detections_gdf.geom_left.intersection(overlap_detections_gdf.geom_right)
+    # Test if left geom is into right geom
+    det_geom = overlap_detections_gdf.geom_left.tolist()
+    iou = []
+    for (i, ii) in zip(intersected_geoms, det_geom):
+        iou.append(metrics.intersection_over_union(i, ii))
+    overlap_detections_gdf['IoU_left'] = iou
+    # Test if right geom is into left geom
+    det_geom = overlap_detections_gdf.geom_right.tolist()
+    iou = []
+    for (i, ii) in zip(intersected_geoms, det_geom):
+        iou.append(metrics.intersection_over_union(i, ii))
+    overlap_detections_gdf['IoU_right'] = iou
+
+    intersecting_dets_gdf = pd.concat(
+        [
+            overlap_detections_gdf[['merged_id_left', 'IoU_left']].groupby(['merged_id_left'], as_index=False).max().rename(
+                columns={'merged_id_left':'merged_id', 'IoU_left':'second_IoU'}
+            ), 
+            overlap_detections_gdf[['merged_id_right', 'IoU_right']].groupby(['merged_id_right'], as_index=False).max().rename(
+                columns={'merged_id_right':'merged_id', 'IoU_right':'second_IoU'}
+            )
+        ],
+        ignore_index=True
+    )
+    intersecting_dets_gdf.sort_values('second_IoU', inplace=True, ascending=False)
+    intersecting_dets_gdf.drop_duplicates(subset='merged_id', inplace=True)
+
+    # Remove dets under another det
+    filtered_groupped_dets_gdf = pd.merge(filtered_groupped_dets_gdf, intersecting_dets_gdf, how='left', on='merged_id')
+    condition = (filtered_groupped_dets_gdf.second_IoU<0.9) | filtered_groupped_dets_gdf.second_IoU.isna()
+    final_groupped_dets_gdf = filtered_groupped_dets_gdf[condition]
+    removed_dets_gdf = pd.concat([removed_dets_gdf, filtered_groupped_dets_gdf[~condition]], ignore_index=True)
+
     filepath = os.path.join(OUTPUT_DIR, 'groupped_detections.gpkg')
-    groupped_detections_gdf.to_file(filepath, driver='GPKG', index=False)
+    final_groupped_dets_gdf.to_file(filepath, crs='EPSG:2056', driver='GPKG', index=False)
     written_files.append(filepath)
 
-    logger.success(f"{DONE_MSG} {len(groupped_detections_gdf)} features were found.")
+    filepath = os.path.join(OUTPUT_DIR, 'removed_detections.gpkg')
+    removed_dets_gdf.to_file(filepath, crs='EPSG:2056', driver='GPKG', index=False)
+    written_files.append(filepath)
+
+    logger.success(f"{DONE_MSG} {len(final_groupped_dets_gdf)} features were kept.")
+    logger.success(f"{len(removed_dets_gdf)} features were removed.")
 
     logger.info('The following files were written:')
     for written_file in written_files:
