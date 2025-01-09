cade3df35f57fc721221876f11d921f96c29a3be
GwenaelleSa <gwenaelle.salamin@gmail.com>
2025-01-09 15:55:11 +0000

    Add filter about area ratio and compactness

--- scripts/filter_detections.py
+++ scripts/filter_detections.py
@@ -56,9 +56,12 @@ if __name__ == "__main__":
     WORKING_DIR = cfg['working_directory']
     AOI = cfg['aoi']
     DETECTIONS = cfg['detections']
+    DEM = cfg['dem']
+
     CANTON = cfg['canton']
     CANTON_PARAMS = cfg['infos'][CANTON]
     LAYERS_DIR = cfg['infos']['layers_directory'].replace('{canton}', CANTON)
+
     AGRI_AREA = none_if_undefined(CANTON_PARAMS, 'agri_area') 
     BUILDINGS = none_if_undefined(CANTON_PARAMS, 'buildings') 
     BUILD_AREAS = none_if_undefined(CANTON_PARAMS, 'building_areas') 
@@ -74,11 +77,13 @@ if __name__ == "__main__":
     ZONE_COMPATIBLE_LPN = none_if_undefined(CANTON_PARAMS, 'zone_compatible_LPN') 
     ZONE_COMPATIBLE_LPN_EXTENSIVE = none_if_undefined(CANTON_PARAMS, 'zone_compatible_LPN_extensive') 
     ZONE_NON_COMPATIBLE_LPN = none_if_undefined(CANTON_PARAMS, 'zone_non_compatible_LPN')
+
     ATTRIBUTE_NAMES = cfg['attribute_names']
     EXCLUSION = cfg['exclusion'] if 'exclusion' in cfg.keys() else None
-    DEM = cfg['dem']
     SCORE_THD = cfg['score_threshold']
     AREA_THD = cfg['area_threshold']
+    AREA_RATIO_THD = cfg['area_ratio_threshold']
+    COMPACTNESS_THD = cfg['compactness_threshold']
     ELEVATION_THD = cfg['elevation_threshold']
 
     os.chdir(WORKING_DIR)
@@ -94,7 +99,7 @@ if __name__ == "__main__":
     detections_gdf = detections_gdf.to_crs(2056)
     if 'tag' in detections_gdf.keys():
         detections_gdf = detections_gdf[detections_gdf['tag']!='FN']
-    detections_gdf['area'] = detections_gdf.geometry.area 
+    detections_gdf['original_area'] = detections_gdf.geometry.area 
     detections_gdf['det_id'] = detections_gdf.index
     total = len(detections_gdf)
     logger.info(f"{total} detections")
@@ -206,6 +211,8 @@ if __name__ == "__main__":
     'large_rivers': large_rivers_gdf, 'protected_area': protected_gdf, 'protected_water': protected_water_gdf, 
     'sda': sda_gdf, 'polluted_sites': polluted_sites_gdf, 'waters': waters_gdf, 'zone_compatible_LPN': zone_compatible_lpn_gdf, 
     'zone_compatible_LPN_extensive': zone_compatible_lpn_extensive_gdf, 'zone_non_compatible_LPN': zone_non_compatible_lpn_gdf}
+    del slope_gdf, agri_gdf, building_gdf, building_areas_gdf, climatic_areas_gdf, forests_gdf, large_rivers_gdf, protected_gdf, protected_water_gdf
+    del polluted_sites_gdf, waters_gdf, zone_compatible_lpn_gdf, zone_compatible_lpn_extensive_gdf, zone_non_compatible_lpn_gdf
 
     logger.info('Control altitude...')
     # Discard polygons detected at/below 0 m and above the threshold elevation and above a given slope
@@ -224,7 +231,8 @@ if __name__ == "__main__":
 
     # Filter dataframe by score value
     check_gdf_len(detections_gdf)
-    detections_score_gdf = detections_gdf[detections_gdf.score > SCORE_THD]
+    SCORE = 'merged_score' if 'merged_score' in detections_gdf.columns else 'score'
+    detections_score_gdf = detections_gdf[detections_gdf[SCORE] > SCORE_THD]
     sc = len(detections_score_gdf)
     logger.info(f"{tdem - sc} detections were removed by score filtering (score threshold = {SCORE_THD})")
 
@@ -245,10 +253,12 @@ if __name__ == "__main__":
         exclu_gdf = exclu_gdf.dissolve()
         detections_gdf = detections_gdf.overlay(exclu_gdf, how='difference', keep_geom_type=False)
 
+    del exclu_gdf, detections_score_gdf
+
     # Spatial join between detections and other vector layers
     check_gdf_len(detections_gdf)
     logger.info('Compute intersection overlap between detection polygons and other vector layer polygons.')
-    detections_infos_gdf = detections_gdf.copy()
+    all_detections_infos_gdf = detections_gdf.copy()
     for key in infos_dict.keys():
         if infos_dict[key].empty:
             pass
@@ -260,31 +270,36 @@ if __name__ == "__main__":
             detections_join_gdf = gpd.sjoin(detections_temp_gdf, gdf[[f'{key}_geom', 'geometry']], how='left', predicate='intersects')  
             del detections_temp_gdf
             del gdf
-            detections_infos_gdf = pd.merge(detections_infos_gdf, detections_join_gdf[[f'{key}_geom', 'det_id']], on='det_id', how='left')
+            all_detections_infos_gdf = pd.merge(all_detections_infos_gdf, detections_join_gdf[[f'{key}_geom', 'det_id']], on='det_id', how='left')
             del detections_join_gdf
-            detections_infos_gdf[f'{key}'] = detections_infos_gdf.apply(lambda x: misc.overlap(x['geometry'], x[f'{key}_geom']) if x['geometry'] and x[f'{key}_geom'] != None else 0, axis=1)
-            detections_infos_gdf = detections_infos_gdf.drop(columns=[f'{key}_geom'])
-            key_list = detections_infos_gdf.columns.values.tolist()
-            detections_infos_gdf = detections_infos_gdf.groupby(by=key_list[:-1], as_index=False).agg({key: ['sum']}).droplevel(1, axis=1) 
-        detections_infos_gdf = gpd.GeoDataFrame(detections_infos_gdf, crs=detections_gdf.crs, geometry='geometry')
+            all_detections_infos_gdf[f'{key}'] = all_detections_infos_gdf.apply(lambda x: misc.overlap(x['geometry'], x[f'{key}_geom']) if x['geometry'] and x[f'{key}_geom'] != None else 0, axis=1)
+            all_detections_infos_gdf = all_detections_infos_gdf.drop(columns=[f'{key}_geom'])
+            key_list = all_detections_infos_gdf.columns.values.tolist()
+            all_detections_infos_gdf = all_detections_infos_gdf.groupby(by=key_list[:-1], as_index=False).agg({key: ['sum']}).droplevel(1, axis=1) 
+        all_detections_infos_gdf = gpd.GeoDataFrame(all_detections_infos_gdf, crs=detections_gdf.crs, geometry='geometry')
 
+    logger.info('Control the area left after spatial difference...')
     # Discard polygons with area under a given threshold 
-    check_gdf_len(detections_infos_gdf)
-    detections_infos_gdf = detections_infos_gdf.explode(ignore_index=True)
-    detections_infos_gdf['area'] = detections_infos_gdf.area
-    tsjoin = len(detections_infos_gdf)
-    detections_infos_gdf = detections_infos_gdf[detections_infos_gdf.area > AREA_THD]
+    check_gdf_len(all_detections_infos_gdf)
+    all_detections_infos_gdf['valid_area'] = all_detections_infos_gdf.area
+    tsjoin = len(all_detections_infos_gdf)
+    detections_infos_gdf = all_detections_infos_gdf[all_detections_infos_gdf.valid_area > AREA_THD]
     ta = len(detections_infos_gdf)
     logger.info(f"{tsjoin - ta} detections were removed by area filtering (area threshold = {AREA_THD} m2)")
 
+    detections_infos_gdf['area_ratio'] = round(detections_infos_gdf.valid_area / detections_infos_gdf.original_area, 2)
+    detections_infos_gdf['compactness'] = 4*np.pi*detections_infos_gdf.area / (detections_infos_gdf.length**2)  # Polsby–Popper test
+    detections_infos_gdf = detections_infos_gdf[(detections_infos_gdf.area_ratio > AREA_RATIO_THD) | (detections_infos_gdf.compactness > COMPACTNESS_THD)]
+    ar = len(detections_infos_gdf)
+    logger.info(f"{ta - ar} detections were removed by area ratio filtering (area ratio threshold = {AREA_RATIO_THD})")
     check_gdf_len(detections_infos_gdf)
 
     # Compute the nearest distance between detections and sda
     if SDA:
         logger.info('Compute the nearest distance between detection polygons and SDA polygons.')
         detections_infos_gdf = gpd.sjoin_nearest(detections_infos_gdf, sda_gdf[['sda_id', 'geometry']], how='left', distance_col='distance_sda')
-        detections_infos_gdf = detections_infos_gdf.drop_duplicates(subset=['det_id', 'sda'])
-        detections_infos_gdf = detections_infos_gdf.drop(columns=['index_right', 'sda_id']) 
+        detections_infos_gdf.drop_duplicates(subset=['det_id', 'sda'], inplace=True)   # Remove duplicates of dets overlapping several SDAs
+        detections_infos_gdf.drop(columns=['index_right', 'sda_id'], inplace=True)
 
     # Final gdf
     logger.info(f"{len(detections_infos_gdf)} detections remaining after filtering")
@@ -293,16 +308,18 @@ if __name__ == "__main__":
     attribute_names_df = pd.read_excel(ATTRIBUTE_NAMES, sheet_name=CANTON, engine='openpyxl')
     for i in range(len(attribute_names_df)):
         if attribute_names_df['argument'][i] in detections_infos_gdf.keys():
-            detections_infos_gdf = detections_infos_gdf.rename(columns={attribute_names_df['argument'][i]: attribute_names_df['name'][i]})
+            detections_infos_gdf.rename(columns={attribute_names_df['argument'][i]: attribute_names_df['name'][i]}, inplace=True)
 
     # Formatting the output name of the filtered detection  
-    feature = f'{DETECTIONS[:-5]}_score-{SCORE_THD}_area-{int(AREA_THD)}_elevation-{int(ELEVATION_THD)}'.replace('0.', '0dot') + '.gpkg'
-    detections_infos_gdf.to_file(feature)
-
+    feature = os.path.join(
+        os.path.dirname(DETECTIONS), 
+        f'dets_{SCORE}-{SCORE_THD}_A-{int(AREA_THD)}_A_ratio-{AREA_RATIO_THD}_C-{COMPACTNESS_THD}_elev-{int(ELEVATION_THD)}'.replace('0.', '0dot') + '.gpkg'
+    )
+    detections_infos_gdf.round(3).to_file(feature)
     written_files.append(feature)
     logger.success(f"{DONE_MSG} A file was written: {feature}")  
     logger.success(f"{DONE_MSG} {len(detections_infos_gdf)} features were kept.")
-    logger.success(f'The covered area is {round(detections_infos_gdf.unary_union.area/1000000)} km2.')
+    logger.success(f'The covered area is {round(detections_infos_gdf.unary_union.area/1000000, 2)} km2.')
 
     logger.info("The following files were written. Let's check them out!")
     for written_file in written_files:
