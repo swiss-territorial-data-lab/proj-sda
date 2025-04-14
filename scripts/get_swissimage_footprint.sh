9239f5358925103f94d4f1dc5e472cf8f897f986
GwenaelleSa <gwenaelle.salamin@gmail.com>
2025-04-14 15:44:32 +0200

    Improve use of bash for initial data

--- scripts/get_swissimage_footprint.sh
+++ scripts/get_swissimage_footprint.sh
@@ -3,5 +3,5 @@
 
 mkdir -p data/AoI/swissimage_footprint
 wget https://data.geo.admin.ch/ch.swisstopo.swissimage-product.metadata/data.zip -P data/AoI/swissimage_footprint
-unzip data/AoI/swissimage_footprint/data.zip
+unzip data/AoI/swissimage_footprint/data.zip -d data/AoI/swissimage_footprint
 rm data/AoI/swissimage_footprint/data.zip
